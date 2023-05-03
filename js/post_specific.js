e24763d7f70b3852318e8778fd0284bec93779ca
Tina <tina.bekkholt@hotmail.com>
2023-05-03 12:50:21 +0200

    small design changes

--- js/post_specific.js
+++ js/post_specific.js
@@ -8,14 +8,28 @@ let id = params.get("id");
 
 const fullPostsURL = api + postsURL;
 
+console.log(id)
+
 async function fetchSinglePost(id) {
     
-    const response = await fetch(postsURL + `${id}`);
+    const response = await fetch(fullPostsURL + `${id}`);
 
     const result = await response.json();
 
     return result
 }
 
+console.log(await fetchSinglePost(id));
+
 const singlePost = await fetchSinglePost(id);
 
+for (let i = 0; i < singlePost.lenght; i++) {
+    const post = singlePost[i];
+
+    const titleContainer = document.createElement("h2");
+
+    const title = post.title.rendered;
+
+    titleContainer.append(title);
+
+}
